b0e62f17660638e3670fcc6dda0bb8ce0cca1a7e
Spencer Seale <spencerseale17@gmail.com>
2020-02-28 19:09:30 -0800

    rdy to rock

---
+++ build.sh
@@ -0,0 +1,1 @@
+./gene-map-build.py --file /Users/spencerseale/motif-mark/Figure_1.fasta --motif /Users/spencerseale/motif-mark/Fig_1_motifs.txt
--- gene-map-build.py
+++ gene-map-build.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
 #run from conda activate snake
+import argparse
 import fxns
 import cairo
 
-in_file = "/Users/spencerseale/motif-mark/Figure_1.fasta"
-mot_file = "/Users/spencerseale/motif-mark/Fig_1_motifs.txt"
+#take in user input
+def get_args():
+    parser = argparse.ArgumentParser(description="Build an image of motif and genes")
+    parser.add_argument("-f", "--file", help="Input fasta file", type=str, required=False)
+    parser.add_argument("-m", "--motif", help="File containing motif sequences to be searched", type=str, required=False)
+    return parser.parse_args()
 
-#test motif file
-#mot_file = "./mot_test.txt"
+args = get_args()
+
+#setting input as user defined files
+in_file = args.file
+mot_file = args.motif
 
 #setting surfaces
 surface = cairo.PDFSurface("motif_img.pdf", 1000, 500)
@@ -22,6 +30,7 @@ mot_holder = fxns.motif_list(mot_file)
 #create picture of exons, introns, and motifs
 fxns.build_picture(surface, out_fa, mot_holder)
 
+#building and locating legend
 locator = 30
 for idx, mo in enumerate(mot_holder):
     col1 = (0.7 + idx/11)
@@ -30,4 +39,5 @@ for idx, mo in enumerate(mot_holder):
     fxns.draw_legend(surface, mo, 910, locator, 950, locator, col1, col2, col3)
     locator += 20
 
+#closing the surface
 surface.finish()
